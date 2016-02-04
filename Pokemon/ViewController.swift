ebf64ef1bd58a2c543e7ae42bc84c8470690a3dc
Addison <addisonbennett@gmail.com>
2016-02-04 16:13:39 -0700

    1-3

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -1,17 +1,18 @@
-
 import UIKit
 import GameplayKit
 
 class ViewController: UIViewController {
-
+    
     var pokemonList = [String]()
     var score = 0
     var correctAnswer = 0
-
+    
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
-
+    @IBOutlet var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,16 +30,18 @@ class ViewController: UIViewController {
         }
         askQuestion()
     }
-
+    
     func askQuestion(action: UIAlertAction! = nil) {
-
+        
         // Shuffle pokemonList so the first three indexes are truly random.
         pokemonList = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(pokemonList) as! [String]
-
+        
         // Assign (the now random) strings at index 1..2 to UIImage buttons.
         button1.setImage(UIImage(named: pokemonList[0]), forState: .Normal)
-
-
+        button2.setImage(UIImage(named: pokemonList[1]), forState: .Normal)
+        button3.setImage(UIImage(named: pokemonList[2]), forState: .Normal)
+        
+        
         
         // Generate random number to reference the display title and correct index in pokemonList.
         correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
@@ -54,15 +57,16 @@ class ViewController: UIViewController {
             title = "Nope. Sorry."
             --score
         }
-        let ac = UIAlertController(title: title, message: "Your score is /(score).", preferredStyle: .Alert)
+        myLabel.text = String(score)
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .Alert)
         ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: askQuestion))
         presentViewController(ac, animated: true, completion: nil)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
+    
+    
 }
 
