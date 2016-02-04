4543099cbe5487dac79f8af9c77f3d8f8432a053
annahuffman147 <annahuffman.plc@gmail.com>
2016-02-04 16:07:45 -0700

    fixed two bugs

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -37,7 +37,8 @@ class ViewController: UIViewController {
 
         // Assign (the now random) strings at index 1..2 to UIImage buttons.
         button1.setImage(UIImage(named: pokemonList[0]), forState: .Normal)
-
+        button2.setImage(UIImage(named: pokemonList[1]), forState: .Normal)
+        button3.setImage(UIImage(named: pokemonList[2]), forState: .Normal)
 
         
         // Generate random number to reference the display title and correct index in pokemonList.
@@ -54,7 +55,7 @@ class ViewController: UIViewController {
             title = "Nope. Sorry."
             --score
         }
-        let ac = UIAlertController(title: title, message: "Your score is /(score).", preferredStyle: .Alert)
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .Alert)
         ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: askQuestion))
         presentViewController(ac, animated: true, completion: nil)
     }
